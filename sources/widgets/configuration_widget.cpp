a5dc07ce73f09439273ed1cfe00350478da2e963
lorsiot <haa3200@gmail.com>
2023-01-29 17:24:02 +0300

    psk file filter fix

--- sources/widgets/configuration_widget.cpp
+++ sources/widgets/configuration_widget.cpp
@@ -44,7 +44,6 @@ void ConfigurationWidget::onInit() {
     grid->addChild(meshLabel);
 
     auto meshPicker = MulMakeWidget<MulFilePicker>();
-    //meshPicker->setFilter("PSK (*.psk)|*.psk");
     meshPicker->setFilter("psk");
     meshPicker->addChangePathCallback(meshModelSelect, [this](const std::filesystem::path& path) {
         meshChangedCallbackCollection.call(path);
